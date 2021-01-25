3d751b1964f42edb1476d9fab808d0de10423c06
IgaBorowska <iga.borowska@gmail.com>
2021-01-25 21:22:12 +0100

    players score

---
+++ Pac-Man/Assets/CharacterScore.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CharacterScore : MonoBehaviour
+{
+    public int points = 0;
+    public Text ScoreTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Point"))
+        {
+            Destroy(other.gameObject);
+            AddScore();
+        }
+    }
+
+    void AddScore()
+    {
+        points++;
+        ScoreTxt.text = "POINTS: " + points.ToString();
+    }
+
+}
